13dbbd219fba5a0d79171bc2efd1beb1daaf1130
litmus-zhang <abdulsalamlukmon9@gmail.com>
2023-05-09 07:43:51 +0000

    Day 21 completed in grand style

---
+++ binary-search/day21/d21.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 0, 3, 5, 9, 12}
+	ans := search(nums, 9)
+	fmt.Printf("ans: %v", ans)
+}
+
+func search(nums []int, target int) int {
+	left := 0
+	right := len(nums)
+
+	for left <= right {
+		mid := ((left + right) / 2) + 1
+		fmt.Printf("left: %v , right: %v , mid: %v \n", left, right, mid)
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid 
+		} else {
+			right = mid
+		}
+
+	}
+
+	return -1
+
+}
