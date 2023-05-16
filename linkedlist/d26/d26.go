3e9a9b4ac165c799a9144fc2e87e15462a4f78c8
litmus-zhang <abdulsalamlukmon9@gmail.com>
2023-05-16 10:55:42 +0100

    Day 26 completed

---
+++ linkedlist/d26/d26.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	ans := reverseList(&ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}})
+	fmt.Println(ans)
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	// set prev to nil
+	var prev *ListNode
+	// set curr to head
+	current := head
+
+	// while curr is not nil
+	for current != nil {
+		// set nextTemp to curr.Next
+		nextTemp := current.Next
+		// set current.Next to prev
+		current.Next = prev
+		// set prev to curr
+		prev = current
+		// set curr to nextTemp
+		current = nextTemp
+	}
+	return prev
+}
