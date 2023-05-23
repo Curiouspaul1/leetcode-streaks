2520f23176738b5e7ce5a151054fd485093d1044
litmus-zhang <abdulsalamlukmon9@gmail.com>
2023-05-23 08:09:52 +0100

    Day 31 completed in grand style

---
+++ tree/day31/d31.go
@@ -0,0 +1,20 @@
+package main
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
+	return root
+
+}
