bdf3f11616ead7fa028be1640e335d400dc059bd
litmus-zhang <abdulsalamlukmon9@gmail.com>
2023-05-30 08:21:47 +0000

    day 36 completed with dubs

---
+++ trie/day36/d36.rs
@@ -0,0 +1,36 @@
+struct Trie {
+
+}
+
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl Trie {
+
+    fn new() -> Self {
+        let trie =  &Trie()
+        return trie
+    }
+    
+    fn insert(&self, word: String) {
+        
+    }
+    
+    fn search(&self, word: String) -> bool {
+        
+    }
+    
+    fn starts_with(&self, prefix: String) -> bool {
+        
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * let obj = Trie::new();
+ * obj.insert(word);
+ * let ret_2: bool = obj.search(word);
+ * let ret_3: bool = obj.starts_with(prefix);
+ */
