57ed3494a98164017f75f4b8d3d18868edd8369f
litmus-zhang <abdulsalamlukmon9@gmail.com>
2023-05-31 10:29:07 +0100

    day 37 completed

---
+++ trie/day37/d37.py
@@ -0,0 +1,23 @@
+class WordDictionary:
+
+    def __init__(self):
+        self.word_dict = collections.defaultdict(set)
+    def addWord(self, word):
+        self.word_dict[len(word)].add(word)
+    def search(self, word):
+        if '.' not in word:
+            return word in self.word_dict[len(word)]
+        for w in self.word_dict[len(word)]:
+            for i, c in enumerate(word):
+                if c != '.' and c != w[i]:
+                    break
+            else:
+                return True
+        return False
+        
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
